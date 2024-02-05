dcf993a74e6ec0b46cf37f60d139bbdcc0950999
mma1377 <alijani.mohammadmahdi@gmail.com>
2024-02-05 22:18:19 +0100

    get_cominant_color is implemented

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
 
+
 function uploadImage() {
     let inputElement = document.getElementById('imageInput');
     let file = inputElement.files[0];
     verifyImage(file);
+}
+
+
+function convolution(input, filter) {
+    let res = Array(input.length).fill(0);
+    for (let i = 0; i < input.length; ++i) {
+        for (let j = 0; j < filter.length; j++) {
+            const index = i - Math.floor(filter.length / 2) + j;
+            if (0 <= index && index < input.length) {
+                res[i] += input[index] * filter[j];
+            }
+        }
+    }
+    return res;
+}
+
+function argmax(input) {
+    let argmax= 0;
+    let max = input[0];
+    for (let i = 0; i < input.length; ++i) {
+        if (input[i] > max) {
+            argmax = i;
+            max = input[i];
+        }
+    }
+    return argmax;
+}
+
+function get_dominant_color(imageDataClamped) {
+    let histogram_red = Array(256).fill(0);
+    let histogram_green = Array(256).fill(0);
+    let histogram_blue = Array(256).fill(0);
+    for (let i = 0; i < imageDataClamped.length / 4; ++i) {
+        let alpha = imageDataClamped[i * 4 + 3];
+        if (alpha !== 0) {
+            histogram_red[imageDataClamped[i * 4]]++;
+            histogram_green[imageDataClamped[i * 4 + 1]]++;
+            histogram_blue[imageDataClamped[i * 4 + 2]]++;
+        }
+    }
+    let gaussian_filter = [0.2, 0.4, 0.6, 0.8, 0.9, 1, 0.9, 0.8, 0.6, 0.4, 0.2];
+    let filtered_red_hist = convolution(histogram_red, gaussian_filter);
+    let filtered_green_hist = convolution(histogram_green, gaussian_filter);
+    let filtered_blue_hist = convolution(histogram_blue, gaussian_filter);
+    let res = [0, 0, 0];
+    res[0] = argmax(filtered_red_hist);
+    res[1] = argmax(filtered_green_hist);
+    res[2] = argmax(filtered_blue_hist);
+    return res;
 }
 
 function verifyImage(imageFile) {
@@ -12,6 +62,7 @@ function verifyImage(imageFile) {
             let img = new Image();
             img.src = URL.createObjectURL(imageFile);
             img.onload = function () {
+
                 if (img.width !== 512 || img.height !== 512)
                     alert("Invalid sizes");
                 let canvas = document.createElement('canvas');
@@ -22,6 +73,7 @@ function verifyImage(imageFile) {
                 let imageDataClamped= ctx.getImageData(
                     0, 0, img.width, img.height
                 ).data;
+
                 for (let i = 0; i < imageDataClamped.length / 4; ++i) {
                     let x = i % img.width;
                     let y = Math.floor(i / img.width);
@@ -31,6 +83,9 @@ function verifyImage(imageFile) {
                         break;
                     }
                 }
+
+                console.log(get_dominant_color(imageDataClamped));
+
             }
         }
     }
